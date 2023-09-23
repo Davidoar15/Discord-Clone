ad429c31f702cd23442479b881a462d691371cff
Davidoar15 <davidoar15@gmail.com>
2023-09-23 18:00:21 -0300

    add/(main)_(routes)_page

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-const TestPage = () => {
-    return (
-        <h1 className="text-3xl font-bold text-indigo-500">
-            This is a Route Test using fold "test"
-        </h1>
-    );
-}
-
-export default TestPage;
