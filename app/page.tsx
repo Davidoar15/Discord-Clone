554335ce62d7cbbf65cc1f8b03f1851fe91e4e03
Davidoar15 <davidoar15@gmail.com>
2023-09-23 14:00:53 -0300

    add/Button_ShadcnUI_in_page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold text-indigo-500'>
-      Discord-Clone
-    </h1>
+    <div>
+      <h1 className='text-3xl font-bold text-indigo-500'>
+        Discord-Clone
+      </h1>
+      <Button>
+        Click Me
+      </Button>
+    </div>
   )
 }
