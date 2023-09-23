8bef5cf4950fa0a180509dee34110a9ff2c355ea
Davidoar15 <davidoar15@gmail.com>
2023-09-23 16:32:43 -0300

    Knowing_Folders_Organization_NextJS

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return (
+        <h1 className="text-3xl font-bold text-indigo-500">
+            This is a Route Test using fold "test"
+        </h1>
+    );
+}
+
+export default TestPage;
