bac09476b55331af8f2c10f3deaaad4a4b2b98ea
Davidoar15 <davidoar15@gmail.com>
2023-09-25 19:28:11 -0300

    fix/userId_SchemaPrisma_&_add/ServerCreation_API

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -38,6 +40,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true);
@@ -53,7 +56,15 @@ export const InitialModal = () => {
 
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values);
+
+            form.reset;
+            router.refresh();
+            window.location.reload();
+        } catch(error) {
+            console.log(error);
+        }
     };
 
     if (!isMounted) {
