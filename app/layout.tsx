bcfd3af249c69e8564721c483ddaeb9fe3967f35
Davidoar15 <davidoar15@gmail.com>
2023-10-02 14:33:36 -0300

    add/server_Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
-import { ModalProvider } from '@/components/providers/modal-provider'
+// import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -31,7 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='discord-theme'
           >
-            <ModalProvider />
+            {/*<ModalProvider />*/}
             {children}
           </ThemeProvider>
         </body>
