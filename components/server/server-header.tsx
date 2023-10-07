9d412dc181f3b1756474358274218938188448ac
Davidoar15 <davidoar15@gmail.com>
2023-10-07 15:06:42 -0300

    add/chat_input

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -26,7 +26,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             >
                 <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
                     {server.name}
-                    <ChevronDown className="h-5 w-5 ml-auto"/>
+                    {/*<ChevronDown className="h-5 w-5 ml-auto"/>*/}
                 </button>
             </DropdownMenuTrigger>
 
