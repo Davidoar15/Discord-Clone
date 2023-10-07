cba3fff12ad2b7d20aaa0d7dc83640dcefc12a8b
Davidoar15 <davidoar15@gmail.com>
2023-10-07 16:39:11 -0300

    add/emojis

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -8,8 +8,10 @@ import qs from "query-string";
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "../emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -25,6 +27,7 @@ const formSchema = z.object({
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
     const { onOpen } = useModal();
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -43,6 +46,9 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
             });
 
             await axios.post(url, value);
+
+            form.reset();
+            router.refresh();
         } catch(error) {
             console.log(error);
         };
@@ -72,7 +78,9 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                                         className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <Smile />
+                                        <EmojiPicker 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
